94974b11e9a04d91e9982422d23018100cde4c8f
Veselin Doychev <veselindoi123@gmail.com>
2023-05-12 15:55:31 +0300

    Adde auth user model in settings.py

--- posts_app/posts_app/settings.py
+++ posts_app/posts_app/settings.py
@@ -122,3 +122,5 @@ MEDIA_ROOT = BASE_DIR / 'mediafiles'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.PostsAppUser'
