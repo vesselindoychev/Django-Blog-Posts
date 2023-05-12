741f9bf0c9c78360ddbd8a3fc1e246e396ecf9ab
Veselin Doychev <veselindoi123@gmail.com>
2023-05-12 14:50:09 +0300

    Set up urls and settigs files

--- posts_app/posts_app/settings.py
+++ posts_app/posts_app/settings.py
@@ -30,6 +30,9 @@ INSTALLED_APPS = [
 
     'posts_app.main',
     'posts_app.accounts',
+
+    # 3rd party apps
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -108,6 +111,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- posts_app/posts_app/urls.py
+++ posts_app/posts_app/urls.py
@@ -1,4 +1,5 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -7,3 +8,5 @@ urlpatterns = [
     path('accounts/', include('posts_app.accounts.urls')),
     path('', include('posts_app.main.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
