e0419f538af0631bc78d6762d3ad94a44b4b3bd6
Veselin Doychev <veselindoi123@gmail.com>
2023-05-12 17:07:54 +0300

    Added admin model for profile

--- posts_app/posts_app/accounts/admin.py
+++ posts_app/posts_app/accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from posts_app.accounts.models import Profile
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    pass
