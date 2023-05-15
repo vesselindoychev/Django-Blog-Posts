ac29264440635ea2fcb136d04976c6f911cb1cb1
Veselin Doychev <veselindoi123@gmail.com>
2023-05-15 12:26:35 +0300

    Added urls for register and logout

--- posts_app/posts_app/accounts/urls.py
+++ posts_app/posts_app/accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from posts_app.accounts.views import LoginUserView
+from posts_app.accounts.views import LoginUserView, RegisterUserView, LogoutUserView
 
 urlpatterns = (
     path('login/', LoginUserView.as_view(), name='login'),
+    path('register/', RegisterUserView.as_view(), name='register'),
+    path('logout/', LogoutUserView.as_view(), name='logout'),
 )
