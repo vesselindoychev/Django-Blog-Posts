c3356b3637c97e5521f3ad26f88c1c22633c84ec
Veselin Doychev <veselindoi123@gmail.com>
2023-05-16 15:09:17 +0300

    Added view for like and unlike post

--- posts_app/posts_app/main/views.py
+++ posts_app/posts_app/main/views.py
@@ -53,8 +53,8 @@ def load_post_data_view(request, num_posts):
     upper = num_posts
     lower = upper - visible
     size = Post.objects.all().count()
-
     qs = Post.objects.all()
+
     data = []
     for obj in qs:
         item = {
@@ -64,6 +64,23 @@ def load_post_data_view(request, num_posts):
             'author': obj.user.email,
             'liked': True if request.user in obj.liked.all() else False,
             'likes_count': obj.like_count,
+            'is_creator': obj.user == request.user
         }
         data.append(item)
     return JsonResponse({'data': data[lower:upper], 'size': size})
+
+
+def like_unlike_post_view(request):
+
+    if request.is_ajax():
+        pk = request.POST.get('pk')
+        obj = Post.objects.get(pk=pk)
+
+        if request.user in obj.liked.all():
+            liked = False
+            obj.liked.remove(request.user)
+        else:
+            liked = True
+            obj.liked.add(request.user)
+
+        return JsonResponse({'liked': liked, 'likes_count': obj.like_count})
