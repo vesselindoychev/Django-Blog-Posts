e8af291d266c83573e60e1684b8062a930ead82b
Veselin Doychev <veselindoi123@gmail.com>
2023-05-16 13:08:26 +0300

    Added new urls

--- posts_app/posts_app/main/urls.py
+++ posts_app/posts_app/main/urls.py
@@ -1,5 +1,16 @@
 from django.urls import path
 
-urlpatterns = (
+from posts_app.main.views import HomeView, CreatePostView, PostListView, load_post_data_view, PostDetailsView
 
+urlpatterns = (
+    # Home
+    path('', HomeView.as_view(), name='home'),
+
+    # Posts
+    path('create-post/', CreatePostView.as_view(), name='create-post'),
+    path('posts/', PostListView.as_view(), name='posts list'),
+    path('post-details/<int:pk>/', PostDetailsView.as_view(), name='post-details'),
+
+    # Posts with AJAX
+    path('data/<int:num_posts>/', load_post_data_view, name='post-data'),
 )
