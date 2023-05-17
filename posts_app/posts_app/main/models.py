b0feb4ebef3e7130c5a6ba32f50e4e75803f25f6
Veselin Doychev <veselindoi123@gmail.com>
2023-05-17 16:21:05 +0300

    Created new Blog Post Model

--- posts_app/posts_app/main/models.py
+++ posts_app/posts_app/main/models.py
@@ -8,6 +8,47 @@ from posts_app.common.custom_validators import validate_letters_numbers_space_an
 UserModel = get_user_model()
 
 
+class BlogPost(models.Model):
+    TITLE_MAX_LENGTH = 100
+    TITLE_MIN_LENGTH = 5
+
+    title = models.CharField(
+        max_length=TITLE_MAX_LENGTH,
+        validators=(
+            MinLengthValidator(TITLE_MIN_LENGTH),
+            validate_letters_numbers_space_and_dash,
+        )
+    )
+
+    body = models.TextField()
+
+    liked = models.ManyToManyField(
+        UserModel,
+        blank=True,
+        related_name='blog_liked',
+    )
+
+    updated = models.DateTimeField(
+        auto_now=True,
+    )
+
+    created = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    user = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+    )
+
+    @property
+    def like_count(self):
+        # liked_post_by_user = Post.objects.get(user__in=self.liked)
+        # total_likes = liked_post_by_user.liked.all().count()
+        # return total_likes
+        return self.liked.all().count()
+
+
 class Post(models.Model):
     TITLE_MAX_LENGTH = 100
     TITLE_MIN_LENGTH = 5
