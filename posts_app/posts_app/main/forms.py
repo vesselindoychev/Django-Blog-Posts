13af327e768103e05167d8677b8a66bdd34c2102
Veselin Doychev <veselindoi123@gmail.com>
2023-05-17 16:21:46 +0300

    Added Create form for Blog Model

--- posts_app/posts_app/main/forms.py
+++ posts_app/posts_app/main/forms.py
@@ -1,10 +1,17 @@
 from django import forms
 
-from posts_app.main.models import Post
+from posts_app.main.models import Post, BlogPost
 
 
 class CreatePostForm(forms.ModelForm):
     class Meta:
         model = Post
         # fields = '__all__'
-        exclude = ('user', )
+        fields = ('title', 'body',)
+
+
+class CreateBlogForm(forms.ModelForm):
+    class Meta:
+        model = BlogPost
+        # fields = '__all__'
+        fields = ('title', 'body',)
