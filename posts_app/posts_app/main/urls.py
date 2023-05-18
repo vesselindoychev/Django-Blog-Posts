c9c43eed71067d8b7385a69692231d0a146b3ca4
Veselin Doychev <veselindoi123@gmail.com>
2023-05-18 17:45:58 +0300

    Added new url for loading blog posts

--- posts_app/posts_app/main/urls.py
+++ posts_app/posts_app/main/urls.py
@@ -1,29 +1,22 @@
 from django.urls import path
 
 from posts_app.main.views import HomeView, PostListView, load_post_data_view, PostDetailsView, \
-    like_unlike_post_view, create_post_view, CreatePostView
+    like_unlike_post_view, create_post_view, CreatePostView, create_blog_post_view, load_blog_post_data_view
 
 urlpatterns = (
     # Home
     path('', HomeView.as_view(), name='home'),
 
-    # Posts {
-
-    # Class Based View
+    # Posts with Normal Classed Based Views
     path('create-post/', CreatePostView.as_view(), name='create-post'),
-
-    # AJAX Function Based View
-    path('posts/', create_post_view, name='posts list'),
-    # path('posts/', PostListView.as_view(), name='posts list'),
-
-    # {
-
-
-
     path('post-details/<int:pk>/', PostDetailsView.as_view(), name='post-details'),
 
-    # Posts with AJAX
+    # Posts with Function Based Views and AJAX requests
+    path('posts/', create_post_view, name='posts list'),
+    path('blog-posts/', create_blog_post_view, name='blog-posts-list'),
+
     path('data/<int:num_posts>/', load_post_data_view, name='post-data'),
+    path('blog-data/<int:num_blog_posts>/', load_blog_post_data_view, name='blog-post-data'),
+
     path('like-unlike-post/', like_unlike_post_view, name='like-unlike-post'),
-    # path('create-post-ajax/', create_post_view, name='create-post-ajax'),
 )
