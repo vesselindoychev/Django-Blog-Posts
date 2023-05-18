74bdfe3f2e1c9ffd3c3b818bd65bbbd535bebd89
Veselin Doychev <veselindoi123@gmail.com>
2023-05-18 17:46:59 +0300

    added two new function views for creating and loading the blog posts in template

--- posts_app/posts_app/main/views.py
+++ posts_app/posts_app/main/views.py
@@ -8,7 +8,7 @@ from django.views import generic as views
 
 from posts_app.accounts.models import Profile
 from posts_app.main.forms import CreatePostForm, CreateBlogForm
-from posts_app.main.models import Post
+from posts_app.main.models import Post, Blog
 
 UserModel = get_user_model()
 
@@ -21,10 +21,10 @@ class CreatePostView(auth_mixins.LoginRequiredMixin, views.CreateView):
     model = Post
     form_class = CreatePostForm
     template_name = 'main/create_post.html'
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('create-post')
 
     def form_valid(self, form):
-        # print(form.instance.user)
+        # print(form.instance.user)\
         form.instance.user = self.request.user
         return super().form_valid(form)
 
@@ -49,6 +49,29 @@ class PostDetailsView(views.DetailView):
         context = super().get_context_data(**kwargs)
         context['is_author'] = self.object.user_id == self.request.user.id
         return context
+
+
+def create_blog_post_view(request):
+    form = CreateBlogForm(request.POST or None)
+
+    if request.is_ajax():
+        if form.is_valid():
+            creator = Profile.objects.get(user=request.user)
+            instance = form.save(commit=False)
+            instance.user = creator
+            instance.save()
+            return JsonResponse({
+                'title': instance.title,
+                'body': instance.body,
+                'author': instance.user.user.email,
+                'id': instance.id,
+            })
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'main/blog-posts-list.html', context)
 
 
 def create_post_view(request):
@@ -86,6 +109,30 @@ def load_post_data_view(request, num_posts):
                 'liked': True if request.user in obj.liked.all() else False,
                 'likes_count': obj.like_count,
                 'is_creator': obj.user == request.user
+            }
+            data.append(item)
+        return JsonResponse({'data': data[lower:upper], 'size': size})
+
+
+def load_blog_post_data_view(request, num_blog_posts):
+    if request.is_ajax():
+        visible = 3
+        upper = num_blog_posts
+        lower = upper - visible
+        size = Blog.objects.all().count()
+        qs = Blog.objects.all()
+
+        data = []
+
+        for obj in qs:
+            item = {
+                'id': obj.id,
+                'title': obj.title,
+                'body': obj.body,
+                'author': obj.user.user.email,
+                'liked': True if obj.user in obj.liked.all() else False,
+                'likes_count': obj.like_count,
+                'is_creator': obj.user.user == request.user
             }
             data.append(item)
         return JsonResponse({'data': data[lower:upper], 'size': size})
