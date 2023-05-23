7383fd4c63ea3f6b63cf4b2da71ec9f2066effe1
Veselin Doychev <veselindoi123@gmail.com>
2023-05-23 12:31:50 +0300

    Added url for BlogPostDetails Class Based View

--- posts_app/posts_app/main/urls.py
+++ posts_app/posts_app/main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from posts_app.main.views import HomeView, PostListView, load_post_data_view, PostDetailsView, \
-    like_unlike_post_view, create_post_view, CreatePostView, create_blog_post_view, load_blog_post_data_view
+    like_unlike_post_view, create_post_view, CreatePostView, create_blog_post_view, load_blog_post_data_view, \
+    BlogPostDetailsView
 
 urlpatterns = (
     # Home
@@ -10,6 +11,7 @@ urlpatterns = (
     # Posts with Normal Classed Based Views
     path('create-post/', CreatePostView.as_view(), name='create-post'),
     path('post-details/<int:pk>/', PostDetailsView.as_view(), name='post-details'),
+    path('blog-post-details/<int:pk>/', BlogPostDetailsView.as_view(), name='blog-post-details'),
 
     # Posts with Function Based Views and AJAX requests
     path('posts/', create_post_view, name='posts list'),
