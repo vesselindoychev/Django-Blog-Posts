7ef6843ec73ee7604a3d8aef15a12e2d063198da
Veselin Doychev <veselindoi123@gmail.com>
2023-05-24 16:05:00 +0300

    Changed h1 to h3

--- posts_app/static/posts/blog-post-details.js
+++ posts_app/static/posts/blog-post-details.js
@@ -1,7 +1,28 @@
 console.log('HI DETAILS')
 
 const backBtn = document.getElementById('back-btn');
+const editBtn = document.getElementById(`edit-btn`);
+const editBtnId = editBtn.getAttribute('itemid');
+const spinnerBox = document.getElementById('spinner-box');
+
+console.log(editBtnId);
+console.log(window.location.href + 'data/')
+
 backBtn.addEventListener('click', (e) => {
     e.preventDefault();
     history.back();
+})
+
+$.ajax({
+    type: 'GET',
+    url: `/blog-post-details/${editBtnId}/`,
+    success: function (response) {
+        // console.log(response);
+        setTimeout(() => {
+            spinnerBox.classList.add('not-visible')
+        }, 1000);
+    },
+    error: function (error) {
+        console.log(error);
+    }
 })
