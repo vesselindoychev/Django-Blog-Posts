f515fb7b6557c414b023a14147d6e70789a3f2c4
Veselin Doychev <veselindoi123@gmail.com>
2023-06-16 13:49:53 +0300

    Created js functions for Hiding and Showing Home Background

--- posts_app/static/home-functions.js
+++ posts_app/static/home-functions.js
@@ -1,20 +1,52 @@
 const homeDiv = Array.from(document.getElementsByClassName('home-wrapper'))[0];
 const viewBtn = document.getElementById('view-picture');
 
-backToHomeDivBtn = Array.from(document.getElementsByClassName('back-to-home-view-btn'))[0];
-backToHomeDiv = document.getElementById('back-to-home-view-div');
+const backToHomeDivBtn = Array.from(document.getElementsByClassName('back-to-home-view-btn'))[0];
+const backToHomeDiv = document.getElementById('back-to-home-view-div');
+const footer = Array.from(document.getElementsByTagName('footer'))[0];
+
+const isHidden = () => homeDiv.classList.contains('home-wrapper--hidden');
+
+homeDiv.addEventListener('transitioned', function () {
+    if (isHidden()) {
+        // homeDiv.style.display = 'none';
+        homeDiv.classList.add('not-visible');
+        footer.classList.add('not-visible')
+    }
+})
+
 
 viewBtn.addEventListener('click', e => {
     e.preventDefault();
-    homeDiv.classList.add('not-visible');
-    homeDiv.classList.remove('home-wrapper')
-    backToHomeDiv.hidden = false
+    if (isHidden()) {
+        homeDiv.classList.remove('not-visible');
+        footer.classList.remove('not-visible')
+        setTimeout(() => homeDiv.classList.remove('home-wrapper--hidden'), 0);
+        setTimeout(() => footer.classList.remove('home-wrapper--hidden'), 0);
+    } else {
+        homeDiv.classList.add('home-wrapper--hidden');
+        homeDiv.classList.add('not-visible');
+        footer.classList.add('home-wrapper--hidden');
+        footer.classList.add('not-visible');
+        setTimeout(() => backToHomeDiv.hidden = false, 800)
+    }
 })
 
 backToHomeDivBtn.addEventListener('click', e => {
     e.preventDefault();
+
+    if (isHidden()) {
+        homeDiv.classList.remove('not-visible');
+        footer.classList.remove('not-visible');
+        setTimeout(() => homeDiv.classList.remove('home-wrapper--hidden'), 0);
+        setTimeout(() => footer.classList.remove('home-wrapper--hidden'), 0);
+    } else {
+        homeDiv.classList.add('home-wrapper--hidden');
+        homeDiv.classList.add('not-visible');
+        footer.classList.add('home-wrapper--hidden');
+        footer.classList.add('not-visible');
+    }
     backToHomeDiv.hidden = true;
-    homeDiv.classList.remove('not-visible');
-    homeDiv.classList.add('home-wrapper');
+
 })
 
