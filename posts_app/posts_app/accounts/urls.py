ba50437bb6bec10258dd7da7dac2e79f97b02b69
Veselin Doychev <veselindoi123@gmail.com>
2023-06-16 13:47:23 +0300

    Created urls for edit and details pages

--- posts_app/posts_app/accounts/urls.py
+++ posts_app/posts_app/accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from posts_app.accounts.views import LoginUserView, RegisterUserView, LogoutUserView
+from posts_app.accounts.views import LoginUserView, RegisterUserView, LogoutUserView, ProfileDetailsView, \
+    EditProfileView
 
 urlpatterns = (
     path('login/', LoginUserView.as_view(), name='login'),
     path('register/', RegisterUserView.as_view(), name='register'),
     path('logout/', LogoutUserView.as_view(), name='logout'),
+    path('details/<int:pk>/', ProfileDetailsView.as_view(), name='profile-details'),
+    path('edit/<int:pk>/', EditProfileView.as_view(), name='edit-profile'),
 )
