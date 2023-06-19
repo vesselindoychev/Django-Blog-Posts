71fcc9857340ba98b7c14c3ecd9de9f38fa1ef5c
Veselin Doychev <veselindoi123@gmail.com>
2023-06-19 16:10:01 +0300

    Added Country and City Models in admin.py

--- posts_app/posts_app/accounts/admin.py
+++ posts_app/posts_app/accounts/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 
-from posts_app.accounts.models import Profile
+from posts_app.accounts.models import Profile, Country, City
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
     pass
