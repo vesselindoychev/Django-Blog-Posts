20aa382dfe99f646f3fd6781e1e8538922ff160c
Veselin Doychev <veselindoi123@gmail.com>
2023-06-20 12:09:23 +0300

    Added url for loading cities and countries select

--- posts_app/posts_app/accounts/urls.py
+++ posts_app/posts_app/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from posts_app.accounts.views import LoginUserView, RegisterUserView, LogoutUserView, ProfileDetailsView, \
-    EditProfileView
+    EditProfileView, load_countries_and_cities
 
 urlpatterns = (
     path('login/', LoginUserView.as_view(), name='login'),
@@ -9,4 +9,6 @@ urlpatterns = (
     path('logout/', LogoutUserView.as_view(), name='logout'),
     path('details/<int:pk>/', ProfileDetailsView.as_view(), name='profile-details'),
     path('edit/<int:pk>/', EditProfileView.as_view(), name='edit-profile'),
+
+    path('ajax/load-cities/', load_countries_and_cities, name='ajax-load-cities'),
 )
